c76a793c57818cb1b0153aaaea264528f9bbe05d
unknown <danilo.pgarcia1@CAS411482W1018.senacsp.edu.br>
2016-03-16 21:24:35 -0300

    exemplo 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Conding Standards"
+
+//doc.prestashop.com/display/ps15/coding+standards
+
+//Variaveis
+
+$nome = 'Danilo'
+
+$nota = 2 + 6;
+
+$nome_completo = $nome .'Ferreira';
+
+echo "<br/><br/>Nome: ".$nome_completo;
+
+
+?>
